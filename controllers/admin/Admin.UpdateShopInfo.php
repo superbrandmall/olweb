867f5b6e18ef750ba05c2224dac91af085ceb39b
Sillypig1983 <jun.ma0902@outlook.com>
2018-08-15 17:51:49 +0800

    -

--- controllers/admin/Admin.UpdateShopInfo.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once '../../models/admin/Shop.class.php';
-$controller = new Shop();
-$controller->_updateShop_action();
--- controllers/user/User.ChangeInfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once '../../models/user/ChangeInfo.class.php';
-$controller = new ChangeInfo();
-
-if(isset($_POST['contact_phone_1']))  {
-    $controller->_changeMobile_action();
-}
