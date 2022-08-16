190fddd2d88c7e426b6d043fdc68ca011448d376
majun <majun@172.18.13.162>
2022-08-16 17:43:39 +0800

    -

--- views/assets/base/js/kow-admin/request-detail-admin.js
+++ views/assets/base/js/kow-admin/request-detail-admin.js
@@ -1210,7 +1210,7 @@ function updateRowInvestmentContractAccounttermCommission(v) {
     input.setAttribute("class","form-control money");
     input.setAttribute("id","commissionTaxDeduct_"+count.toLocaleString());
     input.setAttribute("type","text");
-    input.setAttribute("value",(parseFloat(value.deduct) * 100));
+    input.setAttribute("value",(parseFloat(value.taxDeduct) * 100));
     div.appendChild(input);
     var percent = document.createElement("span");
     percent.innerText = "%";
@@ -1226,7 +1226,7 @@ function updateRowInvestmentContractAccounttermCommission(v) {
     input.setAttribute("type","text");
     input.setAttribute("readonly","");
     input.setAttribute("style","border: none");
-    input.setAttribute("value",(parseFloat(value.taxDeduct) * 100));
+    input.setAttribute("value",(parseFloat(value.deduct) * 100));
     div.appendChild(input);
     var percent = document.createElement("span");
     percent.innerText = "%";
