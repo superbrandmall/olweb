a76d4a9ebb306f779b4144404e2b5b5c5df85677
Sillypig1983 <jun.ma0902@outlook.com>
2022-10-16 22:21:10 +0800

    -

--- views/html/lotus-admin/modality.php
+++ views/html/lotus-admin/modality.php
@@ -6,53 +6,23 @@ if(isset($_SESSION['lotus_admin_name']) && $_SESSION['lotus_admin_name'] == '马
 }
 ?>
 <?php $_SESSION['record_url'] = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>
+<style>
+    #modalities a, #modalities a:active, #modalities a:focus, #modalities a:hover {
+        color: #000000;
+    }
+</style>
 <?php include 'sidebar.php'; ?>
 
 <div class="content-wrapper">
     <form id="search-form" class="form-horizontal" role="form" enctype="multipart/form-data">
-        <section class="sub-header" style="height: 120px;">
+        <section class="sub-header" style="height: 40px;">
             <h4>
                 业态
             </h4>
-            <div class="box-header">
-                <div class="box-body">
-                    <div class="col-md-12">
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" style="text-align: right;">铺位</label>
-                                <div class="col-md-8 col-sm-12" style="text-align: left;">
-                                    <input class="form-control" id="unit" placeholder="代码/名称" type="text"  />
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" style="text-align: right;">租用状态</label>
-                                <div class="col-md-8 col-sm-12" style="text-align: left;">
-                                    <select class="select2" id="shopStatus" style="width: 100%">
-                                        <option value="">未选择</option>
-                                        <option value="1">空闲</option>
-                                        <option value="0">租用</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label class="col-md-4 control-label"></label>
-                                <div class="col-md-8 col-sm-12" style="text-align: left;">
-                                    <button type="button" class="btn btn-info btn-sm" id="search"><i class="fa fa-search icon-white"></i> <span class="hidden-xs">搜索</span></button>
-                                    <button type="button" class="btn btn-default btn-sm" id="clear"><i class="fa fa-times icon-white"></i> <span class="hidden-xs">清除</span></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </section>
     </form>
 
-    <section class="content" style="margin-top: 170px;">
+    <section class="content" style="margin-top: 90px;">
         <div id="webui">
             <div class="row">
                 <div class="col-md-12">
@@ -61,28 +31,26 @@ if(isset($_SESSION['lotus_admin_name']) && $_SESSION['lotus_admin_name'] == '马
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="bootstrap-table">
-                                        <div class="fixed-table-container">
-                                            <div class="fixed-table-body">
-                                                <table class="table table-striped snipe-table table-responsive" style="margin-top: 0; text-align: left;">
-                                                    <thead id="assetsListingTable-sticky-header">
-                                                        <tr>
-                                                            <th>
-                                                                <div class="th-inner">业态</div>
-                                                                <div class="fht-cell"></div>
-                                                            </th>
-                                                            <th>
-                                                                <div class="th-inner">状态</div>
-                                                                <div class="fht-cell"></div>
-                                                            </th>
-                                                            <th>
-                                                                <div class="th-inner">说明</div>
-                                                                <div class="fht-cell"></div>
-                                                            </th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody id="modalities"></tbody>
-                                                </table>
-                                            </div>
+                                        <div class="fixed-table-body">
+                                            <table class="table table-responsive" style="margin-top: 0; text-align: left; border: 0 none;">
+                                                <thead id="assetsListingTable-sticky-header">
+                                                    <tr>
+                                                        <th style="padding: 8px;">
+                                                            <div class="th-inner">业态</div>
+                                                            <div class="fht-cell"></div>
+                                                        </th>
+                                                        <th style="padding: 8px;">
+                                                            <div class="th-inner">状态</div>
+                                                            <div class="fht-cell"></div>
+                                                        </th>
+                                                        <th style="padding: 8px;">
+                                                            <div class="th-inner">说明</div>
+                                                            <div class="fht-cell"></div>
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody id="modalities"></tbody>
+                                            </table>
                                         </div>
                                     </div>
                                     <div class="clearfix"></div>
