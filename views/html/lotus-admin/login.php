32269dd1ac65aee00cb82f9c9cdbee06c98089c9
majun <majun@172.18.13.162>
2022-10-21 16:36:06 +0800

    -

--- views/html/lotus-admin/login.php
+++ views/html/lotus-admin/login.php
@@ -3,14 +3,8 @@
 ?>
 
 <style>
-    .main-header .navbar, .main-header .logo {
-        background-color: #0da5be;
-        background: -webkit-linear-gradient(top,  #0da5be 0%,#0da5be 100%);
-        background: linear-gradient(to bottom, #0da5be 0%,#0da5be 100%);
-        border-color: #0da5be;
-    }
-    .skin-blue .sidebar-menu > li:hover > a, .skin-blue .sidebar-menu > li.active > a {
-        border-left-color: #0da5be;
+    body {
+        background: #ecf0f5;
     }
 
     .btn-primary {
@@ -25,25 +19,25 @@
         font-weight: bold;
         background: #1a2226;
     }
-    
-    .jumbotron {
-        background: #E43C24;
-    }
 </style>
 <body class="hold-transition login-page">
-<div class="jumbotron hero-spacer">
+<div class="jumbotron hero-spacer" style="background: url(/views/assets/base/img/content/lotus-admin/center-top-bj.jpg) 0 0 no-repeat; background-color: #cb220a;">
     <div style="width: 360px; margin: 0 auto;">
         <img id="login-logo" src="/views/assets/base/img/content/lotus-admin/logo.png">
-        <h5 style="display: inline-block; vertical-align: middle; color: #fff;">卜蜂莲花招商管理系统</h5>
+        <h3 style="display: inline-block; vertical-align: bottom; color: #fff; margin-left: 8px;">集成安全系统</h3>
+        <img class="img-responsive" src="/views/assets/base/img/content/lotus-admin/slogan.png">
     </div>
 </div>
 <form id="login_form">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 col-md-offset-4" style="background: #ecf0f5;">
+            <div class="col-md-4">
                 <div class="box login-box">
                     <div class="box-header" style="background: #fff;">
                         <h3 class="box-title"> 用户登录</h3>
+                    </div>
+                    <div class="sub-header" style="display: none;">
+                        <h4>登录 - [正大集团卜蜂莲花]</h4>
                     </div>
                     <div class="login-box-body">
                         <div class="row">
@@ -78,6 +72,28 @@
                         <button type="submit" class="btn btn-lg btn-primary btn-block">登录</button>
                     </div>
                 </div>
+            </div>
+            <div class="col-md-6 col-md-offset-1">
+                <div style="margin: 20px 20px 150px;">
+                    <h5>应用系统：</h5>
+                    <div class="col-md-12">
+                        <div class="col-xs-4 text-center" style="margin-bottom: 10px;">
+                            <img src="/views/assets/base/img/content/lotus-admin/lotus.png" width="50"><br>
+                            莲花招商管理
+                        </div>
+                        <div class="col-xs-4 text-center" style="margin-bottom: 10px;">
+                            <img src="/views/assets/base/img/content/lotus-admin/lotus.png" width="50"><br>
+                            快闪日常管理
+                        </div>
+                        <div class="col-xs-4 text-center" style="margin-bottom: 10px;">
+                            <img src="/views/assets/base/img/content/lotus-admin/lotus.png" width="50"><br>
+                            DR商务审批
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                提示：为获得最佳性能，推荐使用火狐、谷歌等浏览器。<br>
+                建议使用1024*768以上分辨率浏览。
             </div>
         </div>
     </div>
